8e3319cf72fb368a517d0b76ddbe4cdc4d056304
Sahar <s.farahzad91@gmail.com>
2024-09-27 19:37:22 +0330

    Separated settings

--- manage.py
+++ manage.py
@@ -6,7 +6,23 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_mentor.settings')
+    # Get DJANGO_ENV or default to 'local'
+    env = os.environ.setdefault('DJANGO_ENV', 'local')
+
+    # Map environments to their respective settings
+    settings_module = {
+        'production': 'code_mentor.settings.production',
+        'staging': 'code_mentor.settings.staging',
+        'local': 'code_mentor.settings.local',
+    }.get(env)
+
+    # Raise error if DJANGO_ENV is invalid
+    if settings_module is None:
+        raise ValueError(f"Invalid DJANGO_ENV value: {env}. Must be 'production', 'staging', or 'local'.")
+
+    # Set the appropriate settings module
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
